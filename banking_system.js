d7f703e6d62e9e1f263c107927e93b42104895e6
milhamash <85390206+milhamash@users.noreply.github.com>
2023-10-06 10:49:41 +0700

    Penambahan private dan setTimeout

--- banking_system.js
+++ banking_system.js
@@ -1,31 +1,53 @@
-class rekeningBank {    
-    constructor(noRekening, saldo) {
+class Human {
+    constructor(nama, alamat, kode_pin) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.kode_pin = kode_pin;
+    }
+
+    introduce() {
+        console.log(`Hi, ${this.nama} dari ${this.alamat}`)
+    }
+
+    #kodePin = () => {
+        console.log(`Kode PIN anda adalah ${this.kode_pin}`)
+    }
+    mintaPin() {
+        console.log(this.#kodePin());
+    }
+    static #isHidingArea = true;
+}
+
+class rekeningBank extends Human{        
+    constructor(nama, alamat, kode_pin, noRekening, saldo) {
+        super(nama, alamat, kode_pin)
         this.noRekening = noRekening;
         this.saldo = saldo;
-        console.log(`A/c No.: ${noRekening}`);
-        console.log(`Saldo awal: Rp${saldo}`);    
+    }
+    introduce() {
+        super.introduce();
+        console.log(`No. Rekening anda adalah ${this.noRekening} dengan saldo awal Rp${this.saldo}`)
     }
     setor(jumlah) {
         this.saldo += jumlah;
-        console.log(`Setor: Rp${jumlah}`);
+        setTimeout(() => {console.log(`Saldo yang anda setorkan berjumlah Rp${jumlah}`)}, 500);
     }
     ditarik(jumlah) {
         if (jumlah <= this.saldo) {
             this.saldo -= jumlah;
-            console.log(`Ditarik: Rp${jumlah}`);
+            setTimeout(() => {console.log(`Saldo yang anda tarik berjumlah Rp${jumlah}`)}, 500);
         } else {
-            console.log(`Ingin ditarik: Rp${jumlah}`);
-            console.log('Saldo tidak mencukupi');
+            setTimeout(() => {console.log(`Saldo yang ingin anda tarik berjumlah Rp${jumlah}`)}, 500);
+            setTimeout(() => {console.log('Maaf saldo anda tidak mencukupi')}, 500);
         }
     }
     displaySaldo() {
-        console.log(`Saldo Rekening: Rp${this.saldo}`);
+        setTimeout(() => {console.log(`Jumlah saldo rekening anda adalah Rp${this.saldo}`)}, 1000);
     }
 }
 
-const rekening = new rekeningBank('SB-123', 100000);
+let rekening = new rekeningBank('Ilham', 'Bandung', '123456', 'SB-123', 100000);
+rekening.introduce();
 rekening.setor(500000);
-rekening.ditarik(250000);
+rekening.ditarik(650000);
 rekening.displaySaldo();
-
-//alamat, kode pin
